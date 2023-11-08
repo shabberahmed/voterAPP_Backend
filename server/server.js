888de3eeedbb274202c107728f2c120e0653b716
AHMED SHAIK <ahmed@AHMEDs-MacBook-Pro.local>
2023-11-08 13:30:40 +0530

    lkl

--- server/server.js
+++ server/server.js
@@ -5,8 +5,10 @@ import conn from './database/conn.js';
 import route from './routes/AuthRouter.js';
 const PORT=1001
 const app=express()
-app.use(cors('*'))
-app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  }));app.use(express.json())
 conn()
 app.use(route)
 app.listen(PORT,()=>console.log(`server started on port number ${PORT}`))
