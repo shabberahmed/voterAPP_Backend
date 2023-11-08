a649fd40dfb0081d48d11e6a2a7beafa051526e0
AHMED SHAIK <ahmed@AHMEDs-MacBook-Pro.local>
2023-11-08 13:25:31 +0530

    lkk

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import conn from './database/conn.js';
 import route from './routes/AuthRouter.js';
 const PORT=1001
 const app=express()
-app.use(cors())
+app.use(cors('*'))
 app.use(express.json())
 conn()
 app.use(route)
